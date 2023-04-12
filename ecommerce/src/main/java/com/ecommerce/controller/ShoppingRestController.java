1e7eb582d8bc677a76770c5b4b7c6024eb552179
prashanth.kumar <prashanth.kumar@LV-SL1141.lvchn.com>
2023-04-11 20:27:27 -0700

    Adding dto's and post mapping methods

--- ecommerce/src/main/java/com/ecommerce/controller/ShoppingRestController.java
+++ ecommerce/src/main/java/com/ecommerce/controller/ShoppingRestController.java
@@ -1,18 +1,27 @@
 package com.ecommerce.controller;
 
 import java.util.List;
+import java.util.Random;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.ecommerce.dto.OrderDTO;
+import com.ecommerce.dto.ResponseOrderDTO;
+import com.ecommerce.entity.Customer;
+import com.ecommerce.entity.Order;
 import com.ecommerce.entity.Product;
 import com.ecommerce.service.CustomerService;
 import com.ecommerce.service.OrderService;
 import com.ecommerce.service.ProductService;
+import com.ecommerce.util.DateUtil;
 
 @RestController
 @RequestMapping("/api")
@@ -38,6 +47,44 @@ public class ShoppingRestController {
         List<Product> productList = productService.getAllProducts();
 
         return ResponseEntity.ok(productList);
+    }
+    
+    @GetMapping(value = "/getOrder/{orderId}")
+    public ResponseEntity<Order> getOrderDetails(@PathVariable int orderId) {
+
+        Order order = orderService.getOrderDetail(orderId);
+        return ResponseEntity.ok(order);
+    }
+
+
+    @PostMapping("/placeOrder")
+    public ResponseEntity<ResponseOrderDTO> placeOrder(@RequestBody OrderDTO orderDTO) {
+        logger.info("Request Payload " + orderDTO.toString());
+        ResponseOrderDTO responseOrderDTO = new ResponseOrderDTO();
+        float amount = orderService.getCartAmount(orderDTO.getCartItems());
+
+        Customer customer = new Customer(orderDTO.getCustomerName(), orderDTO.getCustomerEmail());
+        Integer customerIdFromDb = customerService.isCustomerPresent(customer);
+        if (customerIdFromDb != null) {
+            customer.setId(customerIdFromDb);
+            logger.info("Customer already present in db with id : " + customerIdFromDb);
+        }else{
+            customer = customerService.saveCustomer(customer);
+            logger.info("Customer saved.. with id : " + customer.getId());
+        }
+        Order order = new Order(orderDTO.getOrderDescription(), customer, orderDTO.getCartItems());
+        order = orderService.saveOrder(order);
+        logger.info("Order processed successfully..");
+
+        responseOrderDTO.setAmount(amount);
+        responseOrderDTO.setDate(DateUtil.getCurrentDateTime());
+        responseOrderDTO.setInvoiceNumber(new Random().nextInt(1000));
+        responseOrderDTO.setOrderId(order.getId());
+        responseOrderDTO.setOrderDescription(orderDTO.getOrderDescription());
+
+        logger.info("test push..");
+
+        return ResponseEntity.ok(responseOrderDTO);
     }
 	
 
